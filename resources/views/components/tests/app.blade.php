ba96ab79f6eafb371427f108fc940d198a4efb32
yun125 <funyacela22@gmail.com>
2022-12-01 02:16:08 +0900

    名前付きスロット

--- resources/views/components/tests/app.blade.php
+++ resources/views/components/tests/app.blade.php
@@ -20,6 +20,9 @@
     </head>
     
     <body>
+        <header>
+            {{$header}}
+        </header>
         <div class="font-sans text-gray-900 antialiased">
             {{ $slot }}
         </div>
