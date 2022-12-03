f4d0c4eee6bf4e8bd26be8b5f93363930fe99075
yun125 <funyacela22@gmail.com>
2022-12-03 13:38:27 +0900

    変数

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">ヘッダー1</x-slot>
 コンポーネントテスト１　
 
-<x-tests.card title="タイトル" content="本文" />
+<x-tests.card title="タイトル" content="本文" :message="$message"/>
 </x-tests.app>
 
 
