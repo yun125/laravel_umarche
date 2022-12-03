9e692ea0bd261b641f7f7b81cca39c0f72418572
yun125 <funyacela22@gmail.com>
2022-12-03 13:46:31 +0900

    props

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -2,7 +2,8 @@
     <x-slot name="header">ヘッダー1</x-slot>
 コンポーネントテスト１　
 
-<x-tests.card title="タイトル" content="本文" :message="$message"/>
+<x-tests.card title="タイトル1" content="本文1" :message="$message"/>
+<x-tests.card title="タイトル2" />>
 </x-tests.app>
 
 
